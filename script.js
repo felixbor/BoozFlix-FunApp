25888b4a6f5efcae15ca591892bc3a3fceb5832a
ejhem <113192777+ejhem@users.noreply.github.com>
2022-11-29 18:48:28 -0400

    added different api for movies, tested working

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function fetchDrink() {
 function fetchMovie() {
     
     var ingredient= "Gin"
-    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=7f9c672a")
+    fetch("https://api.themoviedb.org/3/movie/550?api_key=85bb4e2df2ca0ae4497039fe74f9a9ba")
 
         .then(function (response) {
             return response.json();
