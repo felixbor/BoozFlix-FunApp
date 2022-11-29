b6cf01b475f691f7b7312702dec058fc533c8689
felixbor <felixbor@gmail.com>
2022-11-29 17:52:35 -0400

    addnt second API

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-fetchWeather()
-function fetchWeather() {
+fetchDrink()
+function fetchDrink() {
     
     var ingredient= "Gin"
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+    }
+
+    http://www.omdbapi.com/?i=tt3896198&apikey=7f9c672a
+    fetchMovie()
+function fetchMovie() {
+    
+    var ingredient= "Gin"
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=7f9c672a")
 
         .then(function (response) {
             return response.json();
