a55a1cceb011f7c041f82d4a75e221d3187476ad
Zi <104781216+ZairaBarry@users.noreply.github.com>
2022-11-29 19:10:05 -0400

    updated HTML file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetchDrink()
 function fetchDrink() {
-    
-    var ingredient= "Gin"
+
+    var ingredient = "Gin"
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
 
         .then(function (response) {
@@ -10,14 +10,14 @@ function fetchDrink() {
         .then(function (data) {
             console.log(data);
         })
-    }
+}
 
-    http://www.omdbapi.com/?i=tt3896198&apikey=7f9c672a
-    fetchMovie()
+http://www.omdbapi.com/?i=tt3896198&apikey=7f9c672a
+fetchMovie()
 function fetchMovie() {
-    
-    var ingredient= "Gin"
-    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=7f9c672a")
+
+    var ingredient = "Gin"
+    fetch("https://api.themoviedb.org/3/movie/550?api_key=85bb4e2df2ca0ae4497039fe74f9a9ba")
 
         .then(function (response) {
             return response.json();
@@ -25,4 +25,4 @@ function fetchMovie() {
         .then(function (data) {
             console.log(data);
         })
-    }
+}
