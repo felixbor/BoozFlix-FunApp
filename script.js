22e189ff70af998a40da4764b9189ad24cc46592
Zi <104781216+ZairaBarry@users.noreply.github.com>
2022-11-29 20:41:42 -0400

    added ids and classes into index.html

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var ingredientId = document.querySelector('#igredient_id')
+
+
 fetchDrink()
 function fetchDrink() {
 
@@ -12,12 +15,12 @@ function fetchDrink() {
         })
 }
 
-http://www.omdbapi.com/?i=tt3896198&apikey=7f9c672a
+
 fetchMovie()
 function fetchMovie() {
-    
-    var ingredient= "Gin"
-    fetch("https://api.themoviedb.org/3/movie/550?api_key=85bb4e2df2ca0ae4497039fe74f9a9ba")
+
+    var ingredient = "Gin"
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=85bb4e2df2ca0ae4497039fe74f9a9ba&with_genres=28&page_1&sort_by=popularity.desc")
 
         .then(function (response) {
             return response.json();
