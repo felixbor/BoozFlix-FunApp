fc76d7880333f05b350df0f7bce70fda5f9f0262
ejhem <113192777+ejhem@users.noreply.github.com>
2022-12-05 14:57:13 -0400

    added search function and cleaned some html

--- script.js
+++ script.js
@@ -1,103 +1,96 @@
+//Drinks section
 
-var ingredient = ""
+var ingredient = "";
 $(".search").on("click", function (event) {
-    event.preventDefault()
-    var ingredient = $(".selectDrink").val()
-    console.log(ingredient)
-    $(".result").empty();
+  event.preventDefault();
+  var ingredient = $(".selectDrink").val();
+  console.log(ingredient);
+  $(".result").empty();
 
-    fetchDrink()
+  fetchDrink();
 
-
-
-    function fetchDrink() {
-
-        console.log(ingredient)
-        fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
-
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
-                displayDrinks(data.drinks)
-            })
-    }
-})
+  function fetchDrink() {
+    console.log(ingredient);
+    fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        displayDrinks(data.drinks);
+      });
+  }
+});
 
 function displayDrinks(data) {
-    if (data.length >10){
-        data.length=10
-    }
-    for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        $(".result").append(`<button>${element.strDrink}</button>`);
-        
-    }
-   
-
-  
-    
+  if (data.length > 10) {
+    data.length = 10;
+  }
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+    $(".result").append(`<button>${element.strDrink}</button>`);
+  }
 }
-$(".result").on("click", selectDrink)
+$(".result").on("click", selectDrink);
 function selectDrink(event) {
-    var name = event.target.textContent
-    fetchRecipe(name)
-    $(".ingrMes").empty();
-    $(".ingr").empty();
-    $(".measure").empty();
-
-
+  var name = event.target.textContent;
+  fetchRecipe(name);
+  $(".ingrMes").empty();
+  $(".ingr").empty();
+  $(".measure").empty();
 }
 
 function fetchRecipe(name) {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
-   
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            displayRecipe(data)
-        })
-
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      displayRecipe(data);
+    });
 }
+
 function displayRecipe(data) {
-   
-    var recipe = data.drinks[0].strInstructions
-    var DrinkImage = data.drinks[0].strDrinkThumb
-    for (const [key, value] of Object.entries(data.drinks[0])) {
-      if(key.includes('strIngredient')){
-        if (value!=null){$(".ingr").append(`<p>${value}</p>`)
-    }
-         console.log(value)
- }
-    }
-    for (const [key, value] of Object.entries(data.drinks[0])) {
-       
-
-        if(key.includes('strMeasure')){
-            
-          if (value!=null){$(".measure").append(`<p>${value}</p>`)
+  var recipe = data.drinks[0].strInstructions;
+  var DrinkImage = data.drinks[0].strDrinkThumb;
+  for (const [key, value] of Object.entries(data.drinks[0])) {
+    if (key.includes("strIngredient")) {
+      if (value != null) {
+        $(".ingr").append(`<p>${value}</p>`);
       }
-           console.log(value)
-   }
+      console.log(value);
+    }
+  }
+  for (const [key, value] of Object.entries(data.drinks[0])) {
+    if (key.includes("strMeasure")) {
+      if (value != null) {
+        $(".measure").append(`<p>${value}</p>`);
       }
+      console.log(value);
+    }
+  }
 
-    console.log(DrinkImage)
-   var picture ="https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg"
-    $(".ingrMes").append(`<p>${recipe}</p>`+`<img src=${DrinkImage}>`);
- 
-    }    
+  console.log(DrinkImage);
+  var picture =
+    "https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg";
+  $(".ingrMes").append(`<p>${recipe}</p>` + `<img src=${DrinkImage}>`);
+}
 
 //Movies Section
 
-var movieAPI =
+var movieAPITest =
   "https://api.themoviedb.org/3/discover/movie?api_key=85bb4e2df2ca0ae4497039fe74f9a9ba&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
 var posterPic = "https://image.tmdb.org/t/p/w500/";
+var searchMovieParam =
+  "https://api.themoviedb.org/3/search/movie?api_key=85bb4e2df2ca0ae4497039fe74f9a9ba&query=";
 var movieSuggestionsView = document.getElementById("movieSuggestionsView");
+var movieSearch = document.getElementById("movieSearch");
+var movieInput = document.getElementById("movieInput");
 
-getSuggestions(movieAPI);
+getSuggestions(movieAPITest);
 
 function getSuggestions(url) {
   fetch(url)
@@ -130,7 +123,15 @@ function showSuggestions(data) {
   });
 }
 
-//Image slide show
+movieSearch.addEventListener("submit", (event) => {
+  event.preventDefault();
+  var searchMovie = movieInput.value;
+  if (searchMovie) {
+    getSuggestions(searchMovieParam + searchMovie);
+  }
+});
+
+// Image slide show
 // let slideIndex = 0;
 // showSlides();
 
@@ -139,15 +140,14 @@ function showSuggestions(data) {
 //   let slides = document.getElementsByClassName("mySlides");
 //   let dots = document.getElementsByClassName("dot");
 //   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
+//     slides[i].style.display = "none";
 //   }
 //   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   if (slideIndex > slides.length) {slideIndex = 1}
 //   for (i = 0; i < dots.length; i++) {
 //     dots[i].className = dots[i].className.replace(" active", "");
 //   }
-//   slides[slideIndex-1].style.display = "block";  
+//   slides[slideIndex-1].style.display = "block";
 //   dots[slideIndex-1].className += " active";
 //   setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
-// */
